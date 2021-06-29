85f6383c93bf5527ddd75bca485e14b2b5cb546a
enzo devoto <enzodevotob@gmail.com>
2021-06-29 12:50:32 -0400

    fetched todo list

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,23 +1,26 @@
 import React, {useState} from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+import {RiCloseCircleLine} from 'react-icons/ri'
 
 
-function Todo() {
+function Todo({todos, completeTodo, removeTodo}) {
 const [edit, setEdit] = useState({
     id:null ,
     value: ''
 })
 
-
-
     return todos.map((todo, index) => (
 
-        <div className={ todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
-            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        <div className={ todo.isComplete ? 'todo-row complete text-start todo' : 'todo-row todo'} key={index}>
+            <div key={todo.id } onClick={() => completeTodo(todo.id)}>
                 {todo.text}
             </div>
-            <div className="icons">
+            <div className="icons">     
+                <RiCloseCircleLine onClick={() => {
+                    removeTodo(todo.id)
+                }}
+                className="delete-icon"/>
                 
             </div> 
         </div>
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,31 +1,78 @@
-import React, {useState} from 'react';
-import TodoForm from './TodoForm';
+import React, { useState, useEffect } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 
 function TodoList() {
-    const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    getRoles("https://assets.breatheco.de/apis/fake/todos/user/anna");
+  }, []);
+  const getRoles = (url, options = {}) => {
+    fetch(url, options)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setTodos(data);
+        console.log(todos);
+      });
+  };
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
+  function actualizarFetch() {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/anna", {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+        getRoles("https://assets.breatheco.de/apis/fake/todos/user/anna");
+      })
+      .catch((error) => {
+        //manejo de errores
+        console.log(error);
+      });
+  }
+
+  const removeTodo = id => {
+      const removeArr = [...todos].filter(todo=>todo.id !== id)
+
+      setTodos(removeArr);
+  }
+  const completeTodo = (id) => {
+    let updatedTodo = todos.map((todo) => {
+      if (todo.id == id) {
+        todo.isComplete = !todo.iscomplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodo);
+  };
 
 
-    const addTodo = todo => {
-        if(!todo.text || /^\s*$/.test(todo.text)){
-            return
-        }
-
-        const newTodos = [todo, ...todos]
-        setTodos(newTodos);
-
-    };
-
-    
-
-
-
-
-    return (
-        <div>
-            <h1>Whats the plan for today?</h1>
-            <TodoForm onSubmit={addTodo}/>
-        </div>
-    )
+  return (
+    <div>
+        <h1 className="text-center text-danger display-1">Todos</h1>
+        <div className="p-5 todosApp">
+      <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}/>
+      </div>
+    </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
